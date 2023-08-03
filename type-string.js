9e320b2483ea90586f4a0f2c2feb297212f5f960
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-02 23:23:45 -0300

    Exemplos da aula 01 - Tipos Primitivos. type-number.js - Trabalhando com Números; | type-string.js - Trabalhando com strings e concatenação; | type-boolean.js - Trabalhando com booleanos e validações.

---
+++ type-string.js
@@ -0,0 +1,31 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá mundo!';
+const senha = 'senhaSuperSegura456!';
+const StringDeNumeros = '34567';
+
+const citacao = "Meu nome é ";
+const meuNome = 'Deivid';
+console.log(citacao);
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template literal
+
+const texto = `Amor é fogo que arde sem se ver
+É ferida que dói e não se sente`;
+
+console.log(texto);
+
+// conversao de codigo
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
