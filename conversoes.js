92ea019917240a84d9c45d59a77684a6bd247934
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-03 23:22:55 -0300

    conversoes.js - Mas exemplos de conversões.

--- conversoes.js
+++ conversoes.js
@@ -2,6 +2,7 @@
 // booleanos
 
 // conversão implicita
+
 const numero = 456;
 const numeroString = '456';
 
@@ -13,8 +14,18 @@ console.log(numero + numeroString);
 // conversão explicita
 //Number()
 //String()
+//.toString()
+
 console.log(numero + Number(numeroString))
 
 const stringNumero = '456a'
 
 console.log(numero + Number(stringNumero));
+
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));
+
+let usuarioConectado = false;
+console.log(String(usuarioConectado));
+usuarioConectado = true;
+console.log(String(usuarioConectado));
