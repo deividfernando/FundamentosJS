9bd535194f118a10caa583d25408d8ab7070e6c7
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-03 23:02:47 -0300

    truthy-falsy.js - Mais detalhes sobre booleanos e como são tratatos os valores null e undefined.

---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// boolean 
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy
+
+console.log(0 == false);
+console.log('' == false);
+console.log(1 == true);
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
