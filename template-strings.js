f1c9f35a68e8a8e3ce8c4704b529aeab5842e9be
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-08 22:46:08 -0300

    template-strings.js - Utilizando a estrutura de template string para concatenação de strings e variáveis

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = 'Deivid';
+const idade = 2023 - 1988;
+const cidadeDeNascimeto = 'Belo Horizonte';
+
+//const apresentacao = 'Meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci na cidade de ' + cidadeDeNascimeto;
+
+const apresentacao = `Meu nome é ${nome}, minha ${idade} é idade e eu nasci na cidade ${cidadeDeNascimeto}`;
+
+console.log(apresentacao);
