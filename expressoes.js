2cd4a4b0e6ef316b71a48d01c4cc2fc2b0deb9b4
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-08 23:38:48 -0300

    expressoes.js - Entendendo a diferença entre declaração e expressões de funções.

---
+++ expressoes.js
@@ -0,0 +1,13 @@
+// expressão de função
+
+//const soma = function(num1, num2) {return num1 + num2};
+//console.log(soma(5,3));
+
+console.log(Apresentar());
+
+function Apresentar() {
+    return 'Olá!';
+}
+
+console.log(soma(5,3));
+const soma = function(num1, num2) {return num1 + num2};
