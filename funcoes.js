c60b8782301a4bc9bc5f559a526a1e357deace1e
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-08 23:04:08 -0300

    funcoes.js - Entendendo como declarar e executar funções no JS

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+// let x = '';
+// console.log(x);
+// x = 'oi';
+
+// Declaração da função - Modo 1
+function ImprimeTexto(texto) {
+    console.log(texto);
+}
+
+// Executa a função 1 ou mais vezes
+
+ImprimeTexto('oi');
+ImprimeTexto('OUTRO TEXTO');
+
+function Soma() {
+     return 2 + 2;
+}
+
+// console.log(Soma());
+
+ImprimeTexto(Soma());
