b10bc63edef1c92cda485072705e4f955a2fbf54
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-08 22:31:38 -0300

    comparacoes.js - Entendendo comparações com == e ===; ternario.js - Como utilizar um if ternário

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto = '5';
+
+console.log(numero == texto);
+console.log(numero === texto);
+
+// typeof
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == só compara o valor
+// === compara o valor e o tipo de dado;
+
+// conversao explicita
+
+Number();
+String();
---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+if (idadeCliente >= idadeMinima) {
+    console.log('cerveja');
+} else {
+    console.log('suco');
+}
+
+console.log(idadeCliente >= idadeMinima ? 'cerveja' : 'suco');
